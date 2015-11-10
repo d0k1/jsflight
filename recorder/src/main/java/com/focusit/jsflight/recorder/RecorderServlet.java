2d303d8a9cc97bb3e317fef398eddb5f9ae80478
dOkI <denis.kirpichenkov@gmail.com>
2015-11-11 01:00:39 +0500

    it works!

--- recorder/src/main/java/com/focusit/jsflight/recorder/RecorderServlet.java
+++ recorder/src/main/java/com/focusit/jsflight/recorder/RecorderServlet.java
@@ -1,5 +1,20 @@
 package com.focusit.jsflight.recorder;
 
-public class RecorderServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/jsflight/recorder" })
+public class RecorderServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("RecorderScript!");
+	}
 }
