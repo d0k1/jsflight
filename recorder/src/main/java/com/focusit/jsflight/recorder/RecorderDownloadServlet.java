6390331f80233cbd80dd0ca7182dbe62f043904a
Denis Kirpichenkov <dkirpichenkov@naumen.ru>
2015-11-16 11:51:00 +0500

    1.6

--- recorder/src/main/java/com/focusit/jsflight/recorder/RecorderDownloadServlet.java
+++ recorder/src/main/java/com/focusit/jsflight/recorder/RecorderDownloadServlet.java
@@ -12,30 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 
 @WebServlet(urlPatterns = { "/jsflight/recorder/download" })
-public class RecorderDownloadServlet extends HttpServlet {
+public class RecorderDownloadServlet extends HttpServlet
+{
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		StringWriter writer = new StringWriter();
-		IOUtils.copy(req.getInputStream(), writer, "UTF-8");
-		String theString = writer.toString();
-		
-		String result = java.net.URLDecoder.decode(theString, "UTF-8");
-		if(result.length()<5){
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-			return;
-		}
-		result = result.substring(5, result.length());
-		byte []data = result.getBytes();
-		String name = "record_" + System.currentTimeMillis() + ".json";
-		resp.setContentType("application/json");
-		resp.setHeader("Content-Transfer-Encoding", "binary");
-		resp.setHeader("Content-Length", ""+data.length);
-		resp.setHeader("Content-Disposition", "attachment; filename=\""+name+"\"");
-		resp.getWriter().print(data);
-		resp.getWriter().flush();
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(req.getInputStream(), writer, "UTF-8");
+        String theString = writer.toString();
+
+        String result = java.net.URLDecoder.decode(theString, "UTF-8");
+        if (result.length() < 5)
+        {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        result = result.substring(5, result.length());
+        byte[] data = result.getBytes();
+        String name = "record_" + System.currentTimeMillis() + ".json";
+        resp.setContentType("application/json");
+        resp.setHeader("Content-Transfer-Encoding", "binary");
+        resp.setHeader("Content-Length", "" + data.length);
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+        resp.getWriter().println(result);
+        resp.getWriter().flush();
+    }
 
 }
