78153d5aa3911d05a66d2d317f4ecfc797aacfb2
Denis Kirpichenkov <dkirpichenkov@naumen.ru>
2016-04-29 18:22:50 +0500

    spring boot skeleton

---
+++ server/src/main/java/com/focusit/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.focusit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Generic Spring Boot Application entry point
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
