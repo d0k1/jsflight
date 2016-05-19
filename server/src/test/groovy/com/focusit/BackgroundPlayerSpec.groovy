9ce8b60bc702eed92101916af105a6a3dc3c5882
dOkI <denis.kirpichenkov@gmail.com>
2016-05-19 09:46:57 +0500

    web player test skeleton

---
+++ server/src/test/groovy/com/focusit/BackgroundPlayerSpec.groovy
@@ -0,0 +1,17 @@
+package com.focusit
+
+import spock.lang.Specification
+
+/**
+ * Created by doki on 18.05.16.
+ */
+class BackgroundPlayerSpec extends Specification {
+
+    def "playing can be paused at any time"() {
+
+    }
+
+    def "playing can be terminated at any time"() {
+
+    }
+}
