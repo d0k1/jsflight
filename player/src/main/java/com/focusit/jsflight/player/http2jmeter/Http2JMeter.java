73d0200a9047877d978b8a9f3078973c9e2c204b
Denis Kirpichenkov <dkirpichenkov@naumen.ru>
2016-08-19 19:27:52 +0500

    converter improvement

--- player/src/main/java/com/focusit/jsflight/player/http2jmeter/Http2JMeter.java
+++ player/src/main/java/com/focusit/jsflight/player/http2jmeter/Http2JMeter.java
@@ -15,6 +15,7 @@ import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.threads.ThreadGroup;
+import org.apache.jmeter.timers.ConstantTimer;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 import org.apache.jorphan.collections.HashTreeTraverser;
@@ -29,10 +30,14 @@ import com.focusit.jsflight.recorder.internalevent.httprequest.HttpRecordInforma
  * This example shows how to convert raw traffic recorded by HttpRecorder to JMeter scenario to reproduce a load
  * Created by dkirpichenkov on 17.08.16.
  */
-public class Http2JMeter {
+public class Http2JMeter
+{
     private static final String GUEST_USER = "GUEST";
 
-    static class RestoredRequest {
+    static class RestoredRequest
+    {
+        long timestampNs;
+
         Map<String, String[]> parameters;
         Integer contentLength;
         String contentType;
@@ -45,7 +50,8 @@ public class Http2JMeter {
         HashMap additional;
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             return "RestoredRequest{" + "parameters=" + parameters + ", contentLength=" + contentLength
                     + ", contentType='" + contentType + '\'' + ", uri='" + uri + '\'' + ", method='" + method + '\''
                     + ", contextPath='" + contextPath + '\'' + ", cookies=" + cookies + ", payload=" + payload
@@ -53,25 +59,29 @@ public class Http2JMeter {
         }
     }
 
-    public List<RestoredRequest> getRequests(String file) throws IOException {
+    public List<RestoredRequest> getRequests(String file) throws IOException
+    {
         List<RestoredRequest> requests = new ArrayList<>();
         FastInput input = new FastInput(new FileInputStream(file));
 
         Kryo kryo = new Kryo();
         kryo.register(HashMap.class, new MapSerializer());
-        while (input.available() > 0) {
+        while (input.available() > 0)
+        {
             InternalEventRecorder.InternalEventRecord record = kryo.readObject(input,
                     InternalEventRecorder.InternalEventRecord.class);
-            HttpRecordInformation information = (HttpRecordInformation) record.data;
+            HttpRecordInformation information = (HttpRecordInformation)record.data;
 
             String tag = new String(record.tag).trim();
 
             if (tag.equalsIgnoreCase(
-                    com.focusit.jsflight.recorder.internalevent.httprequest.HttpRecorder.HTTP_RECORDER_TAG)) {
+                    com.focusit.jsflight.recorder.internalevent.httprequest.HttpRecorder.HTTP_RECORDER_TAG))
+            {
 
                 RestoredRequest request = new RestoredRequest();
 
-                if (information.params != null && information.params.length > 0) {
+                if (information.params != null && information.params.length > 0)
+                {
                     FastInput paramsInput = new FastInput(new ByteArrayInputStream(information.params));
 
                     request.parameters = kryo.readObject(paramsInput, HashMap.class);
@@ -83,30 +93,45 @@ public class Http2JMeter {
                     request.contextPath = kryo.readObjectOrNull(paramsInput, String.class);
                     request.cookies = kryo.readObject(paramsInput, HashMap.class);
                 }
-                if (information.payload != null && information.payload.length > 0) {
+                if (information.payload != null && information.payload.length > 0)
+                {
                     request.payload = new String(information.payload);
-                } else {
+                }
+                else
+                {
 
                 }
                 request.additional = information.additional;
+                request.timestampNs = record.timestampNs;
                 requests.add(request);
-
             }
         }
         return requests;
     }
 
-    public Map<String, List<RestoredRequest>> getRequestsByUsers(List<RestoredRequest> requests) {
+    public String getUserUuidByRequest(RestoredRequest request)
+    {
+        String uuid = (String)request.additional.get("uuid");
+        if (uuid == null)
+        {
+            uuid = GUEST_USER;
+        }
+
+        return uuid;
+    }
+
+    public Map<String, List<RestoredRequest>> getRequestsByUsers(List<RestoredRequest> requests)
+    {
         HashMap<String, List<RestoredRequest>> result = new HashMap<>();
 
-        for (RestoredRequest request : requests) {
-            String uuid = (String) request.additional.get("uuid");
-            if (uuid == null) {
-                uuid = GUEST_USER;
-            }
-            if (uuid != null && uuid.trim().length() > 0) {
+        for (RestoredRequest request : requests)
+        {
+            String uuid = getUserUuidByRequest(request);
+            if (uuid != null && uuid.trim().length() > 0)
+            {
                 List<RestoredRequest> filtered = result.get(uuid);
-                if (filtered == null) {
+                if (filtered == null)
+                {
                     filtered = new ArrayList<>();
                     result.put(uuid, filtered);
                 }
@@ -118,7 +143,68 @@ public class Http2JMeter {
         return result;
     }
 
-    public static void main(String[] args) throws IOException {
+    public void addHttpSample(HashTree txTree, RestoredRequest request, long sampleCounter, String login,
+            long lastTimestamp)
+    {
+        HTTPSampler sample = new HTTPSampler();
+        sample.setProperty(TestElement.GUI_CLASS, "HttpTestSampleGui");
+        sample.setName(sampleCounter + " Sample");
+        sample.setDomain("${host}");
+        sample.setProperty(HTTPSampler.PORT, "${port}");
+        sample.setMethod(request.method);
+        sample.setPath(request.uri);
+        sample.setMethod(request.method);
+        Arguments sampleArgs = new Arguments();
+
+        if (request.payload != null && request.payload.trim().length() > 0)
+        {
+            HTTPArgument arg = new HTTPArgument();
+            arg.setAlwaysEncoded(false);
+            arg.setValue(request.payload);
+            sampleArgs.addArgument(arg);
+            sample.setPostBodyRaw(true);
+        }
+        if (!login.equals(GUEST_USER))
+        {
+            HTTPArgument loginArg = new HTTPArgument();
+            loginArg.setAlwaysEncoded(true);
+            loginArg.setName("processAs");
+            loginArg.setValue(login);
+            sampleArgs.addArgument(loginArg);
+        }
+
+        sample.setArguments(sampleArgs);
+        HashTree sampleTree = txTree.add(sample);
+
+        if (request.headers.size() > 0)
+        {
+            HeaderManager headerManager = new HeaderManager();
+            headerManager.setName(sampleCounter + " Headers");
+            headerManager.setProperty(TestElement.GUI_CLASS, "HeaderPanel");
+
+            for (Map.Entry<String, String> entry : request.headers.entrySet())
+            {
+                if (!entry.getKey().toLowerCase().startsWith("cookie"))
+                {
+                    headerManager.add(new Header(entry.getKey(), entry.getValue()));
+                }
+            }
+            sampleTree.add(headerManager);
+        }
+        long nsDelay = request.timestampNs - lastTimestamp;
+        if (lastTimestamp > 0 && nsDelay > 0)
+        {
+            ConstantTimer timer = new ConstantTimer();
+            timer.setProperty(TestElement.GUI_CLASS, "ConstantTimerGui");
+            timer.setDelay(Long.toString(nsDelay / 1000000));
+            timer.setName(sampleCounter + " Timer for " + timer.getDelay());
+            sampleTree.add(timer);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException
+    {
         Http2JMeter converter = new Http2JMeter();
         List<RestoredRequest> requests = converter.getRequests(args[0]);
         Map<String, List<RestoredRequest>> byUuid = converter.getRequestsByUsers(requests);
@@ -138,42 +224,56 @@ public class Http2JMeter {
         JMeterUtils.setProperty("proxy.cert.directory", new File("").getAbsolutePath());
         hashTree = SaveService.loadTree(new File(pathToTemplate));
 
-        final TestPlan[] plan = {null};
+        final TestPlan[] plan = { null };
         final HashTree[] planTree = new HashTree[1];
 
         long sampleCounter = 0;
 
-        hashTree.traverse(new HashTreeTraverser() {
+        hashTree.traverse(new HashTreeTraverser()
+        {
 
             @Override
-            public void addNode(Object node, HashTree subTree) {
+            public void addNode(Object node, HashTree subTree)
+            {
                 System.out.println("Node: " + node.toString());
-                if (node instanceof TestPlan) {
-                    plan[0] = (TestPlan) node;
+                if (node instanceof TestPlan)
+                {
+                    plan[0] = (TestPlan)node;
                     planTree[0] = subTree;
                 }
             }
 
             @Override
-            public void processPath() {
+            public void processPath()
+            {
             }
 
             @Override
-            public void subtractNode() {
+            public void subtractNode()
+            {
             }
         });
 
-        for (String key : byUuid.keySet()) {
+        long lastTimestampsNs = 0;
+
+        HashMap<String, HashTree> txControllers = new HashMap<>();
+
+        for (String key : byUuid.keySet())
+        {
             ThreadGroup group = new ThreadGroup();
             group.setProperty(TestElement.GUI_CLASS, "ThreadGroupGui");
             group.setName("Group_" + key);
+            group.setNumThreads(1);
+            group.setRampUp(0);
 
             LoopController ctrl = new LoopController();
+            ctrl.setLoops(1);
             group.setSamplerController(ctrl);
 
             HashTree groupTree = hashTree.add(group);
             CookieManager mngr = null;
-            if (!key.equals(GUEST_USER)) {
+            if (!key.equals(GUEST_USER))
+            {
                 mngr = new CookieManager();
                 mngr.setName("Cookies_" + key);
                 mngr.setProperty(TestElement.GUI_CLASS, "CookiePanel");
@@ -184,54 +284,15 @@ public class Http2JMeter {
             txCtrl.setName("Tx_" + key);
             txCtrl.setProperty(TestElement.GUI_CLASS, "TransactionControllerGui");
             HashTree txTree = groupTree.add(txCtrl);
+            txControllers.put(key, txTree);
+        }
 
-            for (RestoredRequest request : byUuid.get(key)) {
-                HTTPSampler sample = new HTTPSampler();
-                sample.setProperty(TestElement.GUI_CLASS, "HttpTestSampleGui");
-                sample.setName(sampleCounter + " Sample");
-                sample.setDomain("${host}");
-                sample.setProperty(HTTPSampler.PORT, "${port}");
-                //sample.setCookieManager(mngr);
-                sample.setMethod(request.method);
-                sample.setPath(request.contextPath);
-                sample.setMethod(request.method);
-                Arguments sampleArgs = new Arguments();
-
-                if (request.payload != null && request.payload.trim().length() > 0) {
-                    HTTPArgument arg = new HTTPArgument();
-                    arg.setAlwaysEncoded(false);
-                    arg.setValue(request.payload);
-                    sampleArgs.addArgument(arg);
-                    sample.setPostBodyRaw(true);
-                }
-                if (!key.equals(GUEST_USER)) {
-                    HTTPArgument loginArg = new HTTPArgument();
-                    loginArg.setAlwaysEncoded(true);
-                    loginArg.setName("loginAs");
-                    loginArg.setValue(key);
-                    sampleArgs.addArgument(loginArg);
-                }
-
-                sample.setFollowRedirects(true);
-                sample.setUseKeepAlive(true);
-                sample.setArguments(sampleArgs);
-                HashTree sampleTree = txTree.add(sample);
-
-                if (request.headers.size() > 0) {
-                    HeaderManager headerManager = new HeaderManager();
-                    headerManager.setName(sampleCounter + " Headers");
-                    headerManager.setProperty(TestElement.GUI_CLASS, "HeaderPanel");
-
-                    for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-                        if (!entry.getKey().toLowerCase().startsWith("cookie")) {
-                            headerManager.add(new Header(entry.getKey(), entry.getValue()));
-                        }
-                    }
-                    sampleTree.add(headerManager);
-                }
-
-                sampleCounter++;
-            }
+        for (RestoredRequest request : requests)
+        {
+            String uuid = converter.getUserUuidByRequest(request);
+            HashTree txTree = txControllers.get(uuid);
+            converter.addHttpSample(txTree, request, sampleCounter++, uuid, lastTimestampsNs);
+            lastTimestampsNs = request.timestampNs;
         }
 
         SaveService.saveTree(hashTree, new FileOutputStream(new File("result.jmx")));
