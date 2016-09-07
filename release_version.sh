9d9ac16e544801205018026110d89e9fafc8a1be
Denis Kirpichenkov <denis.kirpichenkov@gmail.com>
2016-09-07 12:39:31 +0500

    Update release_version.sh

--- release_version.sh
+++ release_version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 CURRENT_DIR=$( pwd )
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-mvn release:prepare release:clean
+mvn -Darguments="-DskipTests" release:prepare release:clean
 git commit -am "New development version"
 cd "${CURRENT_DIR}"
